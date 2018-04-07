68d7dd567467ee86d29caecaa0caa189c4340f2d
Antonio Reid <read.musik@gmail.com>
2018-04-07 00:01:59 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,8 +1,9 @@
 class Helpers
 
   def self.current_user(session)
-    @user = User.find_by(username: params[:username], password: params[:password], balance: params[:balance])
     binding.pry 
+    @user = User.find_by(username: params[:username], password: params[:password], balance: params[:balance])
+  
   end
   
 end
